367879d99a9ca4b4fded70f61c37791689720d9d
baz29 <sergey.remov@gmail.com>
2022-08-01 13:12:02 +0300

    feat: reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let str = 0;
+  if (n < 0) {
+  n = n * (-1);
+  str = String(n).split("").reverse().join("");
+ } 
+ else str = String(n).split("").reverse().join("");
+  return (str); 
 }
